9281ec63ed6c5013dbb5d78509ab8e31ca00ffbe
Cailyn <156715706+caiyyn@users.noreply.github.com>
2025-09-09 21:23:29 +0000

    completed

--- main.cpp
+++ main.cpp
@@ -5,5 +5,16 @@ int main() {
   const double length = 7.2;
   const double width = 1.3;
 
+  //calculations 
+  const double perimeter = 2 * (length + width);
+  const double area = length * width;
+
+  //output 
+  cout << " Rectangle Properties: " << endl;
+  cout << " Length = " << length << endl;
+  cout << " Width = " << width << endl;
+  cout << " Area = " << area << endl;
+  cout << " Perimeter = " << perimeter << endl;
+
 
 }
